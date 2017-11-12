565fb871bbdfb4ac1e567606b068379876c0260b
snach <ksu.snach@gmail.com>
2017-11-13 00:39:48 +0300

    done

--- Communizm/tests/test1.py
+++ Communizm/tests/test1.py
@@ -1,11 +1,44 @@
+import json
+import os
+import signal
+import sys
+import time
+
 from django.core.management import call_command
 from django.test import TestCase
-from django.utils.six import StringIO
+
+from coordinates_sender import send_packet
 
 
 class Test1(TestCase):
     def test_command_output(self):
-        data_file = 'hi'
-        out = StringIO()
-        call_command('communizm', stdin=data_file, stdout=out)
-        print out.getvalue()
+
+        hight = -267
+        control_id = b'EyGeDCfJ'
+        r, w = os.pipe()
+
+        pid = os.fork()
+        self.assertGreaterEqual(pid, 0)
+        if pid > 0:
+            os.close(w)
+            r = os.fdopen(r, 'r', 0)
+            time.sleep(2)
+            send_packet(hight, control_id)
+            os.kill(pid, signal.SIGTERM)
+
+        elif pid == 0:
+            os.close(r)
+            StdOut = sys.stdout.fileno()
+            sys.stdout = os.fdopen(StdOut, 'w', 0)
+            os.dup2(w, StdOut)
+            call_command('communizm', stdin="hi")
+
+        result = r.read().split("\n")
+        response_dict = json.loads(result[4].replace("\'", "\""))
+        keys_in_response = ('event_type', 'control_id', 'lon', 'height', 'time', 'lat')
+
+        for key in keys_in_response:
+            self.assertTrue(response_dict.has_key(key))
+
+        self.assertEqual(hight, response_dict.get("height"))
+        self.assertEqual(control_id, response_dict.get("control_id"))
--- coordinates_sender.py
+++ coordinates_sender.py
@@ -7,7 +7,7 @@ PORT = 38202 #58201
 
 
 # Все данные запакованы в big endian.
-for i in range(5):
+def send_packet(hight, key):
 
     version_event_type = struct.pack('!B', 0x01) # версия протокола
 
@@ -20,9 +20,9 @@ for i in range(5):
     latitude = struct.pack('!i', int(latitude_local * 10000000))# 4 байта широта
     longitude =struct.pack('!i', int(longitude_local * 10000000)) # 4 байта долгота
 
-    elevation = struct.pack('!h', -267) # Высота над уровнем моря в метрах
+    elevation = struct.pack('!h', hight) # Высота над уровнем моря в метрах
 
-    user_key = struct.pack('!8s', b'EyGeDCfJ') #bytes([0x00 ,0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07]) # 8 байт ключа пользователя, полученные при авторизации
+    user_key = struct.pack('!8s', key) #bytes([0x00 ,0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07]) # 8 байт ключа пользователя, полученные при авторизации
 
     data_packet = version_event_type + event_type + event_time + latitude + longitude + elevation + user_key
     # размер пакета 24 байта
@@ -41,6 +41,9 @@ for i in range(5):
         if msglen:
             message = s.recv(msglen)
             print("Message recieved:", message)
+
+    return data_packet
+
 
     
 
